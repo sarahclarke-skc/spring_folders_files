b2f555eccfcc53ad250bc10236d177871ac2343c
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-10-12 20:54:37 +0100

    Folder controller written

--- src/main/java/com/example/files_folders_homework/controllers/FolderController.java
+++ src/main/java/com/example/files_folders_homework/controllers/FolderController.java
@@ -1,4 +1,33 @@
 package com.example.files_folders_homework.controllers;
 
+import com.example.files_folders_homework.models.Folder;
+import com.example.files_folders_homework.repositories.FolderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class FolderController {
+
+    @Autowired
+    FolderRepository folderRepository;
+
+    @GetMapping(value = "/folders")
+    public ResponseEntity<List<Folder>> getAllFolders() {
+        return new ResponseEntity<>(folderRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/folders/{id}")
+    public ResponseEntity getFolderById(@PathVariable Long id) {
+        return new ResponseEntity<>(folderRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/folders")
+    public ResponseEntity postFolder(@RequestBody Folder folder) {
+        folderRepository.save(folder);
+        return new ResponseEntity(folder, HttpStatus.OK);
+    }
 }
